7aa1aa4eff67d7d755d40c0c6167bcf96418d7f1
Idrees-Habibi <agent330@remitchoice.email>
2025-01-28 17:30:14 +0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import router from './router'
+import store from  './store'
+import App from './App.vue'
+
+createApp(App)
+.use(router)
+.use(store)
+.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+   
+   <div class="flex flex-col p-8">
+    <input type="text" class="rounded border-3 border-gray-300 w-full"  placeholder=" Search for Meals"/>
+    <div class="flex gap-4 justify-center m-2">
+      <router-link to="/" v-for="letter of letters" :key="letter">
+      {{ letter }}
+    </router-link>
+
+    </div>
+   </div>
+</template>
+<script>
+import { computed } from 'vue';
+import store from '../store';
+
+export default {
+  setup() {
+    const meals = computed(() => store.state.meals);
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+    return { meals , letters}; 
+  },
+};
+</script>
