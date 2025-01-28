e35ce9a78b0decb5037977fa5eccda797a290237
Idrees-Habibi <agent330@remitchoice.email>
2025-01-28 19:21:06 +0500

    new commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
    <div class="flex flex-col p-8">
     <input type="text" class="rounded border-3 border-gray-300 w-full"  placeholder=" Search for Meals"/>
     <div class="flex gap-4 justify-center m-2">
-      <router-link to="/" v-for="letter of letters" :key="letter">
+      <router-link :to="{name:'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
       {{ letter }}
     </router-link>
 
@@ -11,14 +11,23 @@
    </div>
 </template>
 <script>
-import { computed } from 'vue';
-import store from '../store';
+import { onMounted } from 'vue';
+import axiosClient from '../axiosClient.js';
 
 export default {
   setup() {
-    const meals = computed(() => store.state.meals);
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
-    return { meals , letters}; 
+
+    onMounted(async () => {
+      try {
+        const response = await axiosClient.get('list.php?i=list');
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    });
+
+    return { letters };
   },
 };
 </script>
